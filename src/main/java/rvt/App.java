f30383b54c4ab671e9e363659064dbeccb647903
Jekaterina TRUBAČISTOVA <a240530jt@rvt.lv>
2025-09-24 09:00:36 +0300

    password

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,19 @@
 package rvt;
 
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println(123);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Password?");
+        String pass = "Caput Draconis";
+        String userpass = scanner.nextLine();
+        if (userpass.equals(pass)){
+            System.out.println("Caput Draconis");
+        } 
+        else {
+            System.out.println("Off with you!");
+        }
+        scanner.close();
     }
 }
+
