d805633ae08c363e73e59f4c987ea99ddc4f2070
Jekaterina TRUBAČISTOVA <a240530jt@rvt.lv>
2025-09-24 09:09:11 +0300

    Odd or even

---
+++ src/main/java/rvt/OddEven.java
@@ -0,0 +1,21 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number: ");
+        int i = Integer.valueOf(scanner.nextLine());
+        
+        if (i % 2 == 0) {
+            System.out.println("Number " + i + " is even.");
+        } else {
+            System.out.println("Number " + i + " is odd.");
+        }
+            scanner.close();
+        }
+                
+    }
+    
+
