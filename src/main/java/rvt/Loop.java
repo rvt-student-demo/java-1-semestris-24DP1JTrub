518d7887b06faeb06e1dfd5960b8c0e4c7c23f17
Jekaterina TRUBAČISTOVA <a240530jt@rvt.lv>
2025-09-25 12:44:36 +0300

    pirmais cikls

--- src/main/java/rvt/Loop.java
+++ src/main/java/rvt/Loop.java
@@ -1,10 +1,13 @@
 package rvt;
 
 public class Loop {
-    int number = 5; {
+    public static void main(String[] args) {
+        int number = 5; {
     while (number > 0) {
         System.out.println(number);
         number = number - 1;
+    }
+        System.out.println("Ready!");
     }
     }
 }
