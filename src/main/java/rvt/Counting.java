2055a980a5da5ae023b93089f98fd5455fedfa7a
Jekaterina TRUBAČISTOVA <a240530jt@rvt.lv>
2025-09-26 09:41:54 +0300

    parvietoju "counting" rvt mapee

---
+++ src/main/java/rvt/Counting.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Counting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ievadi skaitli: ");
+        int skaitlis = scanner.nextInt();
+
+        for (int i = 0; i <= skaitlis; i++) {
+            System.out.println(i);
+        }
+
+        scanner.close();
+    }
+}
