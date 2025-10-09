eb717717cf342df7dd1f03f8d1a46e3cba5c3e3e
24DP1JTrub <a240530jt@rvt.lv>
2025-10-09 10:34:53 +0200

    sum of array

---
+++ src/main/java/rvt/SumOfArray.java
@@ -0,0 +1,15 @@
+package rvt;
+public class SumOfArray {
+    public static int sumOfNumbersInArray(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {5, 1, 3, 4, 2};
+        System.out.println(sumOfNumbersInArray(numbers));
+    }
+}
