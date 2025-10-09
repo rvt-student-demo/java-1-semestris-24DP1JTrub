e55146455cac36f7ad8392ffb29ca766b3ae3b82
24DP1JTrub <a240530jt@rvt.lv>
2025-10-09 10:39:49 +0200

    print in stars

---
+++ src/main/java/rvt/Printer.java
@@ -0,0 +1,17 @@
+package rvt;
+public class Printer {
+    
+    public static void printArrayInStars(int[] array) {
+        for (int num : array) {
+            for (int i = 0; i < num; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
+    }
+}
