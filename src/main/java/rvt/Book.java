103cdbd644da42aeb281f3858db9f790c38bfbb1
24DP1JTrub <a240530jt@rvt.lv>
2025-11-21 11:55:08 +0200

    starp kods Books

---
+++ src/main/java/rvt/Book.java
@@ -0,0 +1,75 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Book {
+    // Grāmatas nosaukums
+    private String title;
+
+
+    // Grāmatas lapasspuses
+    private int pages;
+
+    // Grāmatas izdošanas gads
+    private int year;
+
+    // Konstruktors
+    public Book(String title, int pages, int year) {
+        this.title = title;
+        this.pages = pages;
+        this.year = year;
+    }
+
+    // Nosaukuma iegūšana
+    public String getTitle() {
+        return title;
+    }
+
+    // Lappaspuses iegūšana
+    public int getPages() {
+        return pages;
+    }
+
+    // Gada iegūšana
+    public int getYear() {
+        return year;
+    }
+
+    
+
+    // Grāmatas apraksts
+    @Override
+    public String toString() {
+        return "Grāmata: " + title + ", pages: " + pages + ", year: " + year;
+    }
+
+
+    public static void main(String[] args) {
+        // Ievades metoode
+        Book book = new Book("Latvijas vēsture", 350, 2020);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ievadiet grāmatas nosaukumu: ");
+        book.title = scanner.nextLine();
+        System.out.print("Ievadiet grāmatas lappaspuses: ");
+        book.pages = scanner.nextInt();
+        System.out.print("Ievadiet grāmatas izdošanas gadu: ");
+        book.year = scanner.nextInt();
+
+
+
+        // Izveidojam jaunu grāmatu objektu
+        System.out.println("Kādu informāciju par grāmatu vēlaties izvadīt? ");
+        String info = scanner.nextLine();
+        if (info.equals("Visu")) {
+            System.out.println("Grāmata: " + book.getTitle() + ", pages: " + book.getPages() + ", year: " + book.getYear());
+        } else if (info.equals("Nosaukumu")) {
+            System.out.println("Grāmatas nosaukums: " + book.getTitle());
+        } else if (info.equals("Lappaspuses")) {
+            System.out.println("Grāmatas lappaspuses: " + book.getPages());
+        } else if (info.equals("Gadu")) {
+            System.out.println("Grāmatas izdošanas gads: " + book.getYear());
+        }
+         scanner.close();
+    }
+   
+}
